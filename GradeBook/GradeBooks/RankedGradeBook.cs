feb32d3601af85da659fc696d5d6564161d23c4e
Parth Patel <p.patel81@yahoo.com>
2019-02-24 14:41:13 +1300

    Adding overrides for CalculateStatics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,5 +26,27 @@ namespace GradeBook.GradeBooks
                 grades[threshold * 3 - 1] <= averageGrade ? 'C' : 
                 grades[threshold * 4 - 1] <= averageGrade ? 'D' :  'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
